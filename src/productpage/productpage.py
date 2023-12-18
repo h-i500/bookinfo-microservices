7e9de1133931152c5b58a8269c48eda19f60121b
hhiroshell <hhiroshell@gmail.com>
2023-12-18 13:29:27 +0900

    add copyrights to manifests

--- src/productpage/productpage.py
+++ src/productpage/productpage.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 #
-# Copyright 2023 hhiroshell@gmail.com
+#   Copyright 2023 hhiroshell@gmail.com
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
